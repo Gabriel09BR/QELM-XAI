af1f392b89b2a99dec6e0676054f6bc9cc9f0eb4
Gabriel Luiz Limeira Barreto <87402449+Gabriel09BR@users.noreply.github.com>
2026-01-15 22:10:44 -0300

    Add files via upload

---
+++ src/qelm_xai/aliases.py
@@ -0,0 +1,9 @@
+ELM_VARIANTS = [
+    "ELMRelu", "ELMSigmoid", "ELMPoly", "ELMLinear",
+    "ELMTribas", "ELMHardlim", "ELMSine", "ELMTan", "ELMRadbas",
+    "QELMRelu", "QELMSigmoid", "QELMPoly", "QELMLinear",
+    "QELMTribas", "QELMHardlim", "QELMSine", "QELMTan", "QELMRadbas",
+]
+
+for name in ELM_VARIANTS:
+    globals()[name] = type(name, (ELMClassifier,), {})
